524ebe0b023dbc9fc0038402b99847ce9261b5f1
ranj2022 <ranjanaraj2019@gmail.com>
2022-03-23 16:12:36 +0000

    program to print first ten numbers and their squares

---
+++ First.java
@@ -0,0 +1,7 @@
+class First{
+	public static void main (String[] args) {
+		System.out.println("THe first numbers are");
+		for(int i=1;i<=10;i++)
+			System.out.println(i+"- Square - "+(i+i));
+	}
+}
---
+++ Second.java
@@ -0,0 +1,9 @@
+
+public class Second{
+
+	public static void main (String[] args) {
+		System.out.println("THe first numbers are");
+		for(int i=1;i<=10;i++)
+			System.out.println(i+"- Square - "+(i*i));
+	}
+}
