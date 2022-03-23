93e9026ea207dd24ced9c833a6470284e06ea32c
ranj2022 <ranjanaraj2019@gmail.com>
2022-03-23 15:47:29 +0000

    its my first java program

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+import java.lang.*; // default avaiable
+
+class HelloWorld{
+//
+	public static void main(String[] args){  // main - starting point
+
+    int n=123;
+    if(n%2==0)
+    System.out.println(n+ " is Even");
+    else 
+    System.out.println(n+ " is odd");
+
+
+	}
+}
+
+
+
