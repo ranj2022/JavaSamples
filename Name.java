3623d2bf8994cd6fbca730924c5441e34f3c7db8
ranj2022 <ranjanaraj2019@gmail.com>
2022-03-23 16:28:19 +0000

    prints name 10 times with city and other details

---
+++ Name.java
@@ -0,0 +1,9 @@
+class Name{
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++){
+
+	System.out.println( "My name is " +args[0]+ " and I am from "+args[1]);
+	System.out.println( "My sons name is " +args[2]);
+}
+	}
+}
