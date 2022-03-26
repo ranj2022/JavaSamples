dc8feaca7d9464a65a8832991227b2fbe665bd16
ranj2022 <ranjanaraj2019@gmail.com>
2022-03-26 08:56:04 +0000

    Program to find min, max and dipsly array in ascending order using Arrays.sort()  method

---
+++ ArraysDemo.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class ArraysDemo{
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in); // creating Scanner object
+
+		System.out.println("Enter the size of the array");
+        int size=in.nextInt();
+
+		int[] numbers=new int[size];
+
+		System.out.println("Enter "+size+ "elemenst to the array");
+		for(int i=0;i<size;i++){
+			numbers[i]=in.nextInt();
+		}
+
+		int min=numbers[0];
+		int max=numbers[0];
+
+		for(int i=0;i<size;i++){
+
+			if(numbers[i]<min)
+				min=numbers[i];
+
+           if(numbers[i]>max)
+				max=numbers[i];
+
+		}
+
+        System.out.println("MAXIMUM is "+max+" \n MINIMUM is "+min);
+        
+        Arrays.sort(numbers); // sorts an array - java.util.Arrays
+
+        //System.out.println ("SORTED ORDER");
+        for(int x:numbers)  // for each
+        System.out.println(x);
+
+	}
+
+}
