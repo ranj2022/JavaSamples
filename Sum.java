332f0e421b51d30a385888192fa0f2de9dd1aae7
ranj2022 <ranjanaraj2019@gmail.com>
2022-03-26 06:08:09 +0000

    program to read and find sum of two numbers from command line

---
+++ Sum.java
@@ -0,0 +1,11 @@
+class Sum{
+	public static void main(String[] args) {
+
+        int a=Integer.parseInt(args[0]); // int - Integer
+        int b=Integer.parseInt(args[1]);
+
+        int sum=a+b;
+
+		System.out.println("The sum of "+a+" and "+b+" is"+sum);
+	}
+}
