bcb8d9152d7825c165dc87d49b866d5516dabd0f
ranj2022 <ranjanaraj2019@gmail.com>
2022-03-27 08:01:50 +0100

    Test.java is main class / exectable class from which Student objects are created and accesses

---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test{
+	
+	public static void main(String[] args) {
+		
+    Student s1,s2; // references
+
+    s1=new Student(); // Student() - constructor - with default vaules - 0, null, 0.0
+    s2=new Student(); // 
+
+    s1.setStudent();
+    s2.setStudent();
+
+    s1.getStudent();
+    s2.getStudent();
+
+	}
+}
