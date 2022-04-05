ce5a0a594236ed1e4ef4694cabb4d1c8d2765be4
ranj2022 <ranjanaraj2019@gmail.com>
2022-04-05 15:59:14 +0100

    importing packages and creating packages

---
+++ TimeDemo.java
@@ -0,0 +1,23 @@
+package hyd.mnk;
+
+import java.time.*;
+
+public class TimeDemo{
+
+	
+public static void main(String[] args) {
+	
+java.util.Date d1=new java.util.Date(); //FQN
+
+System.out.println(d1);
+
+LocalDate date=LocalDate.now();
+
+System.out.println(date);
+
+
+}
+
+}
+
+// javac -d . TimeDemo.java
